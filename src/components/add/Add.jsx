1e6e855e5a4166499d1ed4a6ec0c915114791f38
alikhanov <alixonovshukurullo22@gmail.com>
2024-10-17 15:07:00 +0500

    138

--- src/components/add/Add.jsx
+++ src/components/add/Add.jsx
@@ -21,7 +21,7 @@ const Add = ({ onClose }) => {
   });
 
   const addData = (data) => {
-    axios.post("http://localhost:8080/api/add", data) // Уберите лишний слэш
+    axios.post("https://13dokon-admin-client.vercel.app/api/add", data) // Уберите лишний слэш
       .then(res => {
         notyf.success("Готово! Продукт теперь в каталоге.")
         onClose()
